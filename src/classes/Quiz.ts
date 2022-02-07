2ec5432d169f68e09276f1ae9dbd06b1985cd3b7
Maximo Guk <62088388+Maximo-Guk@users.noreply.github.com>
2022-02-07 12:18:52 -0400

    gameWin logic

--- src/classes/Quiz.ts
+++ src/classes/Quiz.ts
@@ -3,9 +3,14 @@ import User from './User';
 
 export default class Quiz {
   public static getQuestionForUser(user: User) {
+    if (user.getRoundNumber() + 1 > questions.length) {
+      return JSON.stringify({ gameWin: true });
+    }
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const questionsWithoutAnswer: any = { ...questions[user.getRoundNumber()] };
     delete questionsWithoutAnswer.correctAnswer;
+
     return JSON.stringify(questionsWithoutAnswer);
   }
 
